ae53610fb71654d0da90e86a5ef88784d9b91c91
todatoday <taki.souad-90@hotmail.com>
2025-09-06 14:28:58 +0200

    add link

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 const Logo = () => {
   return (
     <div>
+      <Link href="/">
       <Image src="/LogoImg/logorec.png" alt="logo" height={100} width={85} />
+      </Link>
 
     </div>
   );
